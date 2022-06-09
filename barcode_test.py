019c9a67e9a7ad6d8f0b9233a92d75d3c7c994a9
nxnaxx <noname1260@naver.com>
2022-06-09 23:59:20 +0900

    Modify barcode_test.py

--- barcode_test.py
+++ barcode_test.py
@@ -1,8 +1,12 @@
 import argparse
 from fnmatch import fnmatchcase
+from pickle import TRUE
 import cv2
+import numpy as np
 import os
 import glob
+
+from cv2 import sort
 
 def detectBarcode(img):
   # Calculate edge strength
@@ -14,11 +18,47 @@ def detectBarcode(img):
   # Horizontal orientation; To detect barcode area
   dstx = cv2.subtract(sobel_x, sobel_y)
   # Carry out equalization and thresholding
-  dstx = cv2.equalizeHist(dstx)
-  dstx = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+  dstx = cv2.GaussianBlur(dstx, (7, 7), 0)
+  th, dstx = cv2.threshold(dstx, 120, 200, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
   
   # Carry out a morphology transformation
-  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (55, 11))
+  dstx = cv2.morphologyEx(dstx, cv2.MORPH_CLOSE, kernel)
+  dstx = cv2.erode(dstx, kernel, iterations=5)
+  dstx = cv2.dilate(dstx, kernel, iterations=5)
+  
+  contours, hierarchy = cv2.findContours(dstx, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  if contours:
+    max_X = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+  else:
+    max_X = np.array([[0, 0], [0, 0], [0, 0], [0, 0]])
+  
+  # Vertical orientation; To detect barcode area
+  dsty = cv2.subtract(sobel_y, sobel_x)
+  # Carry out equalization and thresholding
+  dsty = cv2.GaussianBlur(dsty, (7, 7), 0)
+  th, dsty = cv2.threshold(dsty, 120, 200, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+  
+  # Carry out a morphology transformation
+  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 55))
+  dsty = cv2.morphologyEx(dsty, cv2.MORPH_CLOSE, kernel)
+  dsty = cv2.erode(dsty, kernel, iterations=5)
+  dsty = cv2.dilate(dsty, kernel, iterations=5)
+  
+  contours, hierarchy = cv2.findContours(dsty, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  if contours:
+    max_Y = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+  else:
+    max_Y = np.array([[0, 0], [0, 0], [0, 0], [0, 0]])
+
+  # To compare the size of the contours
+  if (len(max_X) > len(max_Y)):
+    rect = cv2.minAreaRect(max_X)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    
+  
+  
   
   
 if __name__ == '__main__':
--- examples/sobel_test.py
+++ examples/sobel_test.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 img = cv2.imread('../img/IMG_9736.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -13,13 +14,27 @@ sobel_y = cv2.convertScaleAbs(sobel_y)
 
 # Horizontal orientation; To detect barcode area
 dstx = cv2.subtract(sobel_x, sobel_y)
-dstx = cv2.equalizeHist(dstx)
-
-#cv2.imwrite('his_hori.jpg', dstx)
-
-dstx = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-
-cv2.imwrite('thre_hori.jpg', dstx)
+# Carry out equalization and thresholding
+dstx = cv2.GaussianBlur(dstx, (7, 7), 0)
+th, dstx = cv2.threshold(dstx, 120, 200, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 # Carry out a morphology transformation
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (55, 11))
+dstx = cv2.morphologyEx(dstx, cv2.MORPH_CLOSE, kernel)
+dstx = cv2.erode(dstx, kernel, iterations=5)
+dstx = cv2.dilate(dstx, kernel, iterations=5)
+
+(contours, hierarchy) = cv2.findContours(dstx, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+if not contours:
+  max_X = np.array([[0, 0], [0, 0], [0, 0], [0, 0]])
+else:
+  max_X = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+
+rect = cv2.minAreaRect(max_X)
+box = cv2.boxPoints(rect)
+box = np.int0(box)
+#cv2.drawContours(dstx, [box], 0, (0, 255, 0), 3)
+#cv2.rectangle(dstx, (box[2][0], box[2][1]), (box[0][0], box[0][1]), (0, 255, 0), 2)
+
+#cv2.imwrite('ero_hori.jpg', dstx)
+print(box[-1])
